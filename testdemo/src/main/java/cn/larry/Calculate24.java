381234fb00e6a143b0e0251f4bf0a759cf7e36f9
lucasfu(付光增) <lucasfu@webank.com>
2020-02-12 16:21:15 +0800

    claculate 24

--- testdemo/src/main/java/cn/larry/Calculate24.java
+++ testdemo/src/main/java/cn/larry/Calculate24.java
@@ -1,47 +1,91 @@
 package cn.larry;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
+/**
+ * 4个数字，加减乘除算24，暴力解法
+ */
 public class Calculate24 {
 
+    static final char[] ca = new char[]{'+','-','*','/'};
+
     public static void main(String[] args) {
-      //  List<Inte>
-        int[] nums = new int[]{1,2,3,4};
+        int[] nums = new int[]{5,2,3,4};
+        Set<Double> results = calculate24(nums);
+        System.out.println(results.size());
+        for(Double r:results){
+            System.out.println(r);
+        }
+    }
+
+    public static Set<Double> calculate24(int[] nums){
         List<List<Integer>> lists = combination(nums);
-
-        for(int i=0;i<3;i++){
-
-            for(char c:ca){
-
+        List<List<Character>> lists1 = opCombination(nums.length-1);
+        Set<Double> results = new HashSet<>();
+        for(List<Integer> integers:lists){
+            for(List<Character> characters :lists1){
+                double result = calcualte(integers,characters);
+                results.add(result);
             }
         }
-        for(List<Integer> list:lists){
+        return results;
+    }
 
+    /**
+     *  计算一种情况
+     * @param nums
+     * @param characters
+     * @return
+     */
+    private static double calcualte(List<Integer> nums,List<Character> characters){
+        if(nums.size()-characters.size()!=1)throw new IllegalStateException();
+        double firstOp = nums.get(0);
+        for(int i = 0;i<characters.size();i++){
+            int secondOp = nums.get(i+1);
+            char op = characters.get(i);
+            firstOp = calculate(firstOp,secondOp,op);
         }
-        System.out.println(lists.size());
-        for(List<Integer> l:lists){
-            for(Integer i:l){
-                System.out.print(i+",");
-            }
-            System.out.println();
+        return firstOp;
+    }
+
+    private static double calculate(double first,double second,char op){
+        switch (op){
+            case '+': return first+second;
+            case '-': return first-second;
+            case '*': return first*second;
+            case '/': return first/second;
+            default: throw new IllegalArgumentException();
         }
     }
 
+
+    /**
+     * 算运算符的排列,递归
+     * @param n
+     * @return
+     */
     public static List<List<Character>> opCombination(int n){
-        char[] ca = new char[]{'+','-','*','/'};
-        List<List<Integer>> lists = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            List<List<Character>> newList = new ArrayList<>();
-            newList.add(Arrays.asList());
+        if(n<=0)return new ArrayList<>();
+        List<List<Character>> lists =new ArrayList<>();
+        if(n == 1 ){
+            for(char c:ca)
+                lists.add(Arrays.asList(c));
+            return lists;
+        }
+        List<List<Character>>  lists1= opCombination(n-1);
+        for(List<Character> lc:lists1){
             for(char c:ca){
-
+               List<Character> clist = new LinkedList<>(lc);
+               clist.add(c);
+               lists.add(clist);
             }
         }
+        return lists;
     }
 
+    /**
+     * 算数字的所有组合，递推，动态规划
+     */
     public static List<List<Integer>> combination(int[] nums){
         List<List<Integer>> lists = new ArrayList<>();
         lists.add(Arrays.asList(nums[0]));
@@ -60,11 +104,4 @@ public class Calculate24 {
         return lists;
     }
 
-    public List<Integer> calculate(int[] nums){
-        List<Integer> integers = new LinkedList<>();
-        for(int num:nums){
-            integers.add(num);
-        }
-return integers;
-    }
 }
